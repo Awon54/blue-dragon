fabe95b03d050d90e3de98327cf008b5ff02db30
Awon <awon54@gmail.com>
2020-10-22 10:22:14 +0545

    WIP patch API

--- blue-dragon/Controllers/API/V1/ActivitiesController.cs
+++ blue-dragon/Controllers/API/V1/ActivitiesController.cs
@@ -1,34 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using blue_dragon.Data;
 using blue_dragon.Models.V1;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace blue_dragon.Controllers.API.V1
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ActivitiesApiController : ControllerBase
+    public class ActivitiesController : ControllerBase
     {
         private readonly SQLiteDbContext _context;
 
-        public ActivitiesApiController(SQLiteDbContext context)
+        public ActivitiesController(SQLiteDbContext context)
         {
             _context = context;
         }
 
-        // GET: api/ActivitiesApi
+        // GET: api/Activities
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _context.Activities.OrderByDescending( x=> x.DateTime ).ToListAsync();
         }
 
-        // GET: api/ActivitiesApi/5
+        // GET: api/Activities/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(int id)
         {
@@ -42,11 +41,56 @@ namespace blue_dragon.Controllers.API.V1
             return activity;
         }
 
-        // PUT: api/ActivitiesApi/5
+        // Patch status: api/Activities/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PutActivity(int id, [FromBody] JsonPatchDocument<Activity> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+            var activityFromDb = await _context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (activityFromDb == null)
+            { 
+                return NotFound();
+            }
+            
+            patchDoc.ApplyTo(activityFromDb, (Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter) ModelState);
+
+            var isValid = TryValidateModel(activityFromDb);
+
+            if (!isValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActivityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+
+        // PUT: api/Activities/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutActivity(int id, Activity activity)
+        public async Task<IActionResult> PatchStatus(int id, [FromBody] Activity activity)
         {
             if (id != activity.Id)
             {
@@ -74,7 +118,7 @@ namespace blue_dragon.Controllers.API.V1
             return NoContent();
         }
 
-        // POST: api/ActivitiesApi
+        // POST: api/Activities
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
@@ -86,7 +130,7 @@ namespace blue_dragon.Controllers.API.V1
             return CreatedAtAction("GetActivity", new { id = activity.Id }, activity);
         }
 
-        // DELETE: api/ActivitiesApi/5
+        // DELETE: api/Activities/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Activity>> DeleteActivity(int id)
         {
